cc303ccef789f25fbf751a6519fb9228553514fc
fabiofontesx <fontes.fabio@outlook.com>
2020-05-03 22:05:55 -0300

    Revert "Alterado key para buscar host"
    This reverts commit de789a94d85d9f227c56b2d11321e863b3f7587e.

--- src/config/database.js
+++ src/config/database.js
@@ -19,7 +19,7 @@ module.exports = {
 
     production: {
         dialect: 'postgres',
-        host: process.env.DATABASE_URL,
+        host: process.env.DB_HOST,
         username: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE
